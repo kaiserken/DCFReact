6d538f6e0bd8bef55aa586ad26c26d83ee1c6606
Ken Kaiser <kkaiser@doublehelixllc.com>
2015-10-26 11:38:33 -0700

    added commas to input cash flows and helper functions

--- client/components/Dcfmodel.js
+++ client/components/Dcfmodel.js
@@ -18,9 +18,21 @@ var Dcfmodel  = React.createClass({
 
   },
 
+
+  numberFormatting: function(event){
+      return (new Intl.NumberFormat({ style: 'currency', currency: 'USD' }).format(event));
+  },
+
+  stringNumber: function(event){
+      return ((event).replace(/[, ]+/g, "").trim());
+  },
+
   changeCashFlow: function(event){
-    var cashflow = event.target.value;
-    //cashflow = new Intl.NumberFormat({ style: 'currency', currency: 'USD' }).format(cashflow);
+    event.target.value = this.stringNumber(event.target.value);
+    console.log(event.target.value);
+    var cashflow = this.numberFormatting(event.target.value);
+    event.target.value = cashflow;
+    //debugger;
     // the above does not actually change the render and makes input values a string
     var id = event.target.id;
     var data = {};
@@ -34,10 +46,15 @@ var Dcfmodel  = React.createClass({
   },
 
   discountedValue: function(cf1,cf2,cf3,cf4,cf5,rate, grwthrate){
-    grwthrate = 1 +grwthrate;
-    rate = 1 + rate ;
+    grwthrate = 1 +grwthrate/100;
+    rate = 1 + rate/100 ;
     console.log("grate", grwthrate);
     console.log('rate', rate);
+    cf1 = this.stringNumber(cf1);
+    cf2 = this.stringNumber(cf2);
+    cf3 = this.stringNumber(cf3);
+    cf4= this.stringNumber(cf4);
+    cf5 = this.stringNumber(cf5);
 
     //console.log((cf5*grwthrate/(rate-grwthrate))/(Math.pow(rate,5)))
     return Math.round((cf1/rate+ cf2/(Math.pow(rate,2))+ cf3/(Math.pow(rate,3))+cf4/(Math.pow(rate,4))+cf5/(Math.pow(rate,5))+
@@ -47,7 +64,7 @@ var Dcfmodel  = React.createClass({
   computeValue: function(event){
     event.preventDefault();
     var companysValue  = this.discountedValue(this.state.cashFlowYear1, this.state.cashFlowYear2, this.state.cashFlowYear3, this.state.cashFlowYear4, this.state.cashFlowYear5, this.state.discountRate, this.state.longTermGrowthRate);
-    companysValue = new Intl.NumberFormat({ style: 'currency', currency: 'USD' }).format(companysValue);
+    companysValue = this.numberFormatting(companysValue);
     this.setState({companyValue : companysValue})
   },
 
@@ -59,17 +76,17 @@ var Dcfmodel  = React.createClass({
       <form  className = "cashflows" onSubmit ={this.computeValue} > In the Boxes below - Input your projected cashflow for each of the next 5 years. A good approximation of cash flow is EBITDA minus average annual capital expenditures. EBITDA is earnings before interest, taxes, depreciation and amortization.
         <br/>
         <br/>
-        <input id = 'cashFlowYear1' type  = "number"  defaultValue = "" placeholder = "Year 1 cashflow" onChange = {this.changeCashFlow}/>
-        <input id  ='cashFlowYear2' type  = "number"  defaultValue = "" placeholder = "Year 2 cashflow" onChange = {this.changeCashFlow}/>
-        <input id  ='cashFlowYear3' type  = "number"  defaultValue = "" placeholder = "Year 3 cashflow" onChange = {this.changeCashFlow}/>
-        <input id  ='cashFlowYear4' type  = "number" defaultValue = "" placeholder = "Year 4 cashflow" onChange = {this.changeCashFlow}/>
-        <input id = 'cashFlowYear5' type  = "number"  defaultValue = "" placeholder = "Year 5 cashflow" onChange = {this.changeCashFlow}/>
+        <input  id = 'cashFlowYear1' type  = "text"  defaultValue = "" placeholder = "Year 1 cashflow" onChange = {this.changeCashFlow}/>
+        <input id  ='cashFlowYear2' type  = "text"  defaultValue = "" placeholder = "Year 2 cashflow" onChange = {this.changeCashFlow}/>
+        <input id  ='cashFlowYear3' type  = "text"  defaultValue = "" placeholder = "Year 3 cashflow" onChange = {this.changeCashFlow}/>
+        <input id  ='cashFlowYear4' type  = "text" defaultValue = "" placeholder = "Year 4 cashflow" onChange = {this.changeCashFlow}/>
+        <input id = 'cashFlowYear5' type  = "text"  defaultValue = "" placeholder = "Year 5 cashflow" onChange = {this.changeCashFlow}/>
         <br/>
         <br/>
-        <p>Enter the Discount Rate as a decimal</p>
+        <p>Enter the Discount Rate </p>
         <input id  = 'discountRate' type  = "text"  defaultValue = "" placeholder = "Discount Rate" onChange = {this.changeCashFlow}/>
         <br/>
-        <p>Enter the Long Term Growth Rate of Cash Flows after year 5 as a decimal</p>
+        <p>Enter the Long Term Growth Rate of Cash Flows after year 5 </p>
         <input id = "longTermGrowthRate" type  = "text"  defaultValue = "" placeholder = "Growth Rate" onChange = {this.changeCashFlow}/>
         <br/>
         <br/>

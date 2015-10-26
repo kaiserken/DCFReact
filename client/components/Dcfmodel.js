58cd7e844672bcef9e13bc4ea7660e87a2da0bcf
Ken Kaiser <kkaiser@doublehelixllc.com>
2015-10-26 00:11:14 -0700

    added function to compute value with user inputs for discount rate etc..

--- client/components/Dcfmodel.js
+++ client/components/Dcfmodel.js
@@ -20,17 +20,25 @@ var Dcfmodel  = React.createClass({
 
   changeCashFlow: function(event){
     var cashflow = event.target.value;
+    //cashflow = new Intl.NumberFormat({ style: 'currency', currency: 'USD' }).format(cashflow);
+    // the above does not actually change the render and makes input values a string
     var id = event.target.id;
     var data = {};
     data[id] = cashflow;
-    this.setState({
-      discountRate : 1.15,
-      longTermGrowthRate:1.02
-    })//rates hard coded for time being
     this.setState(data)
+    // hard keyed right now
+    // this.setState({
+    //   discountRate: 1.2,
+    //   longTermGrowthRate:1.04
+    // })
   },
 
   discountedValue: function(cf1,cf2,cf3,cf4,cf5,rate, grwthrate){
+    grwthrate = 1 +grwthrate;
+    rate = 1 + rate ;
+    console.log("grate", grwthrate);
+    console.log('rate', rate);
+
     //console.log((cf5*grwthrate/(rate-grwthrate))/(Math.pow(rate,5)))
     return Math.round((cf1/rate+ cf2/(Math.pow(rate,2))+ cf3/(Math.pow(rate,3))+cf4/(Math.pow(rate,4))+cf5/(Math.pow(rate,5))+
     (cf5*grwthrate/(rate-grwthrate))/(Math.pow(rate,5))));
@@ -39,6 +47,7 @@ var Dcfmodel  = React.createClass({
   computeValue: function(event){
     event.preventDefault();
     var companysValue  = this.discountedValue(this.state.cashFlowYear1, this.state.cashFlowYear2, this.state.cashFlowYear3, this.state.cashFlowYear4, this.state.cashFlowYear5, this.state.discountRate, this.state.longTermGrowthRate);
+    companysValue = new Intl.NumberFormat({ style: 'currency', currency: 'USD' }).format(companysValue);
     this.setState({companyValue : companysValue})
   },
 
@@ -50,16 +59,23 @@ var Dcfmodel  = React.createClass({
       <form  className = "cashflows" onSubmit ={this.computeValue} > In the Boxes below - Input your projected cashflow for each of the next 5 years. A good approximation of cash flow is EBITDA minus average annual capital expenditures. EBITDA is earnings before interest, taxes, depreciation and amortization.
         <br/>
         <br/>
-        <input id = 'cashFlowYear1' type  = "text"  defaultValue = "" placeholder = "Year 1 cashflow" onChange = {this.changeCashFlow}/>
-        <input id  ='cashFlowYear2' type  = "text"  defaultValue = "" placeholder = "Year 2 cashflow" onChange = {this.changeCashFlow}/>
-        <input id  ='cashFlowYear3' type  = "text"  defaultValue = "" placeholder = "Year 3 cashflow" onChange = {this.changeCashFlow}/>
-        <input id  ='cashFlowYear4' type  = "text" defaultValue = "" placeholder = "Year 4 cashflow" onChange = {this.changeCashFlow}/>
-        <input id = 'cashFlowYear5' type  = "text"  defaultValue = "" placeholder = "Year 5 cashflow" onChange = {this.changeCashFlow}/>
+        <input id = 'cashFlowYear1' type  = "number"  defaultValue = "" placeholder = "Year 1 cashflow" onChange = {this.changeCashFlow}/>
+        <input id  ='cashFlowYear2' type  = "number"  defaultValue = "" placeholder = "Year 2 cashflow" onChange = {this.changeCashFlow}/>
+        <input id  ='cashFlowYear3' type  = "number"  defaultValue = "" placeholder = "Year 3 cashflow" onChange = {this.changeCashFlow}/>
+        <input id  ='cashFlowYear4' type  = "number" defaultValue = "" placeholder = "Year 4 cashflow" onChange = {this.changeCashFlow}/>
+        <input id = 'cashFlowYear5' type  = "number"  defaultValue = "" placeholder = "Year 5 cashflow" onChange = {this.changeCashFlow}/>
+        <br/>
+        <br/>
+        <p>Enter the Discount Rate as a decimal</p>
+        <input id  = 'discountRate' type  = "text"  defaultValue = "" placeholder = "Discount Rate" onChange = {this.changeCashFlow}/>
+        <br/>
+        <p>Enter the Long Term Growth Rate of Cash Flows after year 5 as a decimal</p>
+        <input id = "longTermGrowthRate" type  = "text"  defaultValue = "" placeholder = "Growth Rate" onChange = {this.changeCashFlow}/>
         <br/>
         <br/>
         <button>Calculate Value</button>
       </form>
-      <h4 className = "cashflows">Company Value {this.state.companyValue}</h4>
+      <h4 className = "cashflows">Company Value ${this.state.companyValue}</h4>
       </div>
 
     );
